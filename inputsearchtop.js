ad1576a2482dc454825617157330d8bcbdd288c2
IslamMap <Islammap.net@gmail.com>
2024-07-08 10:44:33 -0400

    Add files via upload

---
+++ inputsearchtop.js
@@ -0,0 +1,7 @@
+        // Change the placeholder value using JavaScript
+        document.addEventListener('DOMContentLoaded', function() {
+            var searchInput = document.getElementById('search-input-top');
+            if (searchInput) {
+                searchInput.placeholder = 'e.g. new placeholder value';
+            }
+        });
