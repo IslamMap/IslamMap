e8dbe97bf38259110db9a0069bce601f9e20a932
IslamMap <Islammap.net@gmail.com>
2024-07-09 10:40:32 -0400

    Delete inputsearchtop.js

--- inputsearchtop.js
+++
@@ -1,9 +0,0 @@
-        // Change the placeholder value using JavaScript
-
-console.log("hello");
-        document.addEventListener('DOMContentLoaded', function() {
-            var searchInput = document.getElementById('search-input-top');
-            if (searchInput) {
-                searchInput.placeholder = 'e.g. new placeholder value';
-            }
-        });
